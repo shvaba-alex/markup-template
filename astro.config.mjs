477e788126339e1e95d330f5147fbc57905e7ce3
shvaba <shvabaaleksandr@gmail.com>
2023-07-19 21:27:33 +0300

    add additional css variables and add eslint + prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'entry.[hash].js',
+                    chunkFileNames: 'chunks/chunk.[hash].js',
+                    assetFileNames: 'assets/asset.[hash][extname]',
+                },
+            },
+        },
+    },
+});
--- dist/chunks/chunk.5cad8636.js
+++ dist/chunks/chunk.5cad8636.js
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="ru">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="description" content="Astro description">
-		<meta name="viewport" content="width=device-width">
-		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
-		<meta name="generator" content="Astro v2.7.0">
-		<title>Welcome to my Template.</title>
-	<link rel="stylesheet" href="/assets/asset.3496cf1a.css" /></head>
-	<body>
-		<header class="page-header">
+import { c as createAstro, a as createComponent, r as renderTemplate, m as maybeRenderHead, b as addAttribute, d as renderHead, e as renderComponent, f as renderSlot } from './chunk.2a6b4664.js';
+import 'html-escaper';
+/* empty css                */
+const $$Astro$4 = createAstro();
+const $$Header = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$4, $$props, $$slots);
+  Astro2.self = $$Header;
+  return renderTemplate`${maybeRenderHead($$result)}<header class="page-header">
     <div class="page-header__container">
         <nav class="page-header__nav" aria-label="Main nav">
             <ul class="page-header__nav-list nav-list">
@@ -41,13 +37,81 @@
             </li>
         </ul>
     </div>
-</header>
-		
-    <main class="page">
+</header>`;
+}, "C:/Users/shvaba/Desktop/projects/markup-template/src/components/Header.astro");
+
+const $$Astro$3 = createAstro();
+const $$Footer = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$3, $$props, $$slots);
+  Astro2.self = $$Footer;
+  return renderTemplate`${maybeRenderHead($$result)}<footer class="page-footer"></footer>`;
+}, "C:/Users/shvaba/Desktop/projects/markup-template/src/components/Footer.astro");
+
+const $$Astro$2 = createAstro();
+const $$Layout = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  Astro2.self = $$Layout;
+  const { title } = Astro2.props;
+  return renderTemplate`<html lang="ru">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="Astro description">
+		<meta name="viewport" content="width=device-width">
+		<link rel="icon" type="image/svg+xml" href="/favicon.svg">
+		<meta name="generator"${addAttribute(Astro2.generator, "content")}>
+		<title>${title}</title>
+	${renderHead($$result)}</head>
+	<body>
+		${renderComponent($$result, "Header", $$Header, {})}
+		${renderSlot($$result, $$slots["default"])}
+		${renderComponent($$result, "Footer", $$Footer, {})}
+		<div class="page-overlay"></div>
+		<div class="page-popups-area"></div>
+	</body></html>`;
+}, "C:/Users/shvaba/Desktop/projects/markup-template/src/layouts/Layout.astro");
+
+const $$Astro$1 = createAstro();
+const $$PortfolioCard = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$PortfolioCard;
+  const { href, tags, title, image } = Astro2.props;
+  return renderTemplate`${maybeRenderHead($$result)}<div class="portfolio-card">
+	<div class="portfolio-card__image-wrapper">
+		<img${addAttribute(image, "src")}${addAttribute(title, "alt")}>
+	</div>
+	<h3 class="portfolio-card__title text-h3">
+		<a${addAttribute(href, "href")} class="portfolio-card__link" target="_blank">${title}</a>
+	</h3>
+	<ul class="portfolio-card__tags">
+		${tags.map((tag) => {
+    return renderTemplate`<li class="portfolio-card__tag">${tag}</li>`;
+  })}
+	</ul>
+</div>`;
+}, "C:/Users/shvaba/Desktop/projects/markup-template/src/components/PortfolioCard.astro");
+
+const volmkImg = "/assets/asset.62ef882f.webp";
+
+const grodnomkImg = "/assets/asset.fa855289.webp";
+
+const mppImg = "/assets/asset.9dc95c09.webp";
+
+const crmImg = "/assets/asset.53702bfe.webp";
+
+const meImg = "/assets/asset.8856bf58.jpg";
+
+const shopnemanImg = "/assets/asset.2e7b0738.webp";
+
+const $$Astro = createAstro();
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Welcome to my Template." }, { "default": ($$result2) => renderTemplate`
+    ${maybeRenderHead($$result2)}<main class="page">
         <section id="intro-section" class="page__hero-section hero-section" aria-labelledby="hero-section-label">
             <div class="hero-section__container">
                 <div class="hero-section__avatar">
-                    <img src="/assets/asset.8856bf58.jpg" alt="Shvaba Alexander">
+                    <img${addAttribute(meImg, "src")} alt="Shvaba Alexander">
                 </div>
                 <div class="hero-section__conten">
                     <h1 class="hero-section__title text-h1" id="hero-section-label">
@@ -99,67 +163,27 @@
                     </h2>
                 </header>
                 <div class="portfolio-section__portfolios">
-                    <div class="portfolio-card">
-	<div class="portfolio-card__image-wrapper">
-		<img src="/assets/asset.53702bfe.webp" alt="CRM (NDA)">
-	</div>
-	<h3 class="portfolio-card__title text-h3">
-		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&#38;ab_channel=RickAstley" class="portfolio-card__link" target="_blank">CRM (NDA)</a>
-	</h3>
-	<ul class="portfolio-card__tags">
-		<li class="portfolio-card__tag">js</li><li class="portfolio-card__tag">vue.js 3</li><li class="portfolio-card__tag">php</li><li class="portfolio-card__tag">laravel</li><li class="portfolio-card__tag">websocket</li><li class="portfolio-card__tag">centrifugo</li><li class="portfolio-card__tag">REST API</li>
-	</ul>
-</div>
-                    <div class="portfolio-card">
-	<div class="portfolio-card__image-wrapper">
-		<img src="/assets/asset.62ef882f.webp" alt="Volkovysk meat-processing plant">
-	</div>
-	<h3 class="portfolio-card__title text-h3">
-		<a href="https://volmk.by/" class="portfolio-card__link" target="_blank">Volkovysk meat-processing plant</a>
-	</h3>
-	<ul class="portfolio-card__tags">
-		<li class="portfolio-card__tag">scss</li><li class="portfolio-card__tag">css</li><li class="portfolio-card__tag">js</li><li class="portfolio-card__tag">gulp</li><li class="portfolio-card__tag">bitrix</li>
-	</ul>
-</div>
-                    <div class="portfolio-card">
-	<div class="portfolio-card__image-wrapper">
-		<img src="/assets/asset.fa855289.webp" alt="Grodnomk">
-	</div>
-	<h3 class="portfolio-card__title text-h3">
-		<a href="https://grodnomk.by" class="portfolio-card__link" target="_blank">Grodnomk</a>
-	</h3>
-	<ul class="portfolio-card__tags">
-		<li class="portfolio-card__tag">scss</li><li class="portfolio-card__tag">css</li><li class="portfolio-card__tag">js</li><li class="portfolio-card__tag">gulp</li><li class="portfolio-card__tag">bitrix</li>
-	</ul>
-</div>
-                    <div class="portfolio-card">
-	<div class="portfolio-card__image-wrapper">
-		<img src="/assets/asset.9dc95c09.webp" alt="MASTERPRINT-PACK">
-	</div>
-	<h3 class="portfolio-card__title text-h3">
-		<a href="https://mpp.by" class="portfolio-card__link" target="_blank">MASTERPRINT-PACK</a>
-	</h3>
-	<ul class="portfolio-card__tags">
-		<li class="portfolio-card__tag">scss</li><li class="portfolio-card__tag">css</li><li class="portfolio-card__tag">js</li><li class="portfolio-card__tag">gulp</li><li class="portfolio-card__tag">wordpress</li>
-	</ul>
-</div>
-                    <div class="portfolio-card">
-	<div class="portfolio-card__image-wrapper">
-		<img src="/assets/asset.2e7b0738.webp" alt="Shop Neman">
-	</div>
-	<h3 class="portfolio-card__title text-h3">
-		<a href="https://shopneman.by/" class="portfolio-card__link" target="_blank">Shop Neman</a>
-	</h3>
-	<ul class="portfolio-card__tags">
-		<li class="portfolio-card__tag">scss</li><li class="portfolio-card__tag">css</li><li class="portfolio-card__tag">js</li><li class="portfolio-card__tag">gulp</li>
-	</ul>
-</div>
+                    ${renderComponent($$result2, "PortfolioCard", $$PortfolioCard, { "href": "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley", "image": crmImg, "tags": [
+    "js",
+    "vue.js 3",
+    "php",
+    "laravel",
+    "websocket",
+    "centrifugo",
+    "REST API"
+  ], "title": "CRM (NDA)" })}
+                    ${renderComponent($$result2, "PortfolioCard", $$PortfolioCard, { "href": "https://volmk.by/", "image": volmkImg, "tags": ["scss", "css", "js", "gulp", "bitrix"], "title": "Volkovysk meat-processing plant" })}
+                    ${renderComponent($$result2, "PortfolioCard", $$PortfolioCard, { "href": "https://grodnomk.by", "image": grodnomkImg, "tags": ["scss", "css", "js", "gulp", "bitrix"], "title": "Grodnomk" })}
+                    ${renderComponent($$result2, "PortfolioCard", $$PortfolioCard, { "href": "https://mpp.by", "image": mppImg, "tags": ["scss", "css", "js", "gulp", "wordpress"], "title": "MASTERPRINT-PACK" })}
+                    ${renderComponent($$result2, "PortfolioCard", $$PortfolioCard, { "href": "https://shopneman.by/", "image": shopnemanImg, "tags": ["scss", "css", "js", "gulp"], "title": "Shop Neman" })}
                 </div>
             </div>
         </section>
     </main>
+` })}`;
+}, "C:/Users/shvaba/Desktop/projects/markup-template/src/pages/index.astro");
 
-		<footer class="page-footer"></footer>
-		<div class="page-overlay"></div>
-		<div class="page-popups-area"></div>
-	</body></html>
+const $$file = "C:/Users/shvaba/Desktop/projects/markup-template/src/pages/index.astro";
+const $$url = "";
+
+export { $$Index as default, $$file as file, $$url as url };
